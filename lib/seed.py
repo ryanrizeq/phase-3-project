a4b9d650e7788fcbc2f7bea163afca7112d66c44
Ryan Rizeq <ryanrizeq@gmail.com>
2023-08-16 18:24:37 -0700

    create dog data

--- lib/seed.py
+++ lib/seed.py
@@ -1,11 +1,20 @@
+from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import Dog, Kennel, Owner
 
+fake = Faker()
+
 engine = create_engine('sqlite:///doggy_daycare.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def create_dogs():
-    pass
+def create_data():
+    dogs = [Dog(
+        name = fake.language_name(),
+        breed = fake.animal.dog()
+    ) for i in range(20)]
+
+    session.add_all(dogs)
+    session.commit()
